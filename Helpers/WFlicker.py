4651c2715afda11b87ee2261c00f4d8da2af9995
Zebrakopf <sch.felix.sch@gmail.com>
2022-06-16 14:43:03 +0100

    clean up and added comments

--- Helpers/WFlicker.py
+++ Helpers/WFlicker.py
@@ -3,27 +3,15 @@ import pygame
 
 class WFlicker(QtWidgets.QWidget):
 
-    def __init__(self, parent, frequency, amplitude):
+    def __init__(self, parent):
         super(WFlicker, self).__init__(parent)
 
 
 
         self.flickerWidth = self.parent().width
         self.flickerHeight = self.parent().height
-        self_frequency = frequency
-        self_amplitude = amplitude
-        self.opac = 100
+        self.opac = 100 # default full opacity
 
-        #self.light.setGeometry(
-            #self.ulx, self.uly, self.lightWidth, self.lightHeight)
-        #self.light.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
-    def paintEvent2(self, e):
-        screen = QtGui.QGuiApplication.screens()[0]
-        # Initialing Color
-        color = (255,0,0)
-        
-        # Drawing Rectangle
-        pygame.draw.rect(screen, color, pygame.Rect(30, 30, 60, 60))
     def paintEvent(self, e):
         self.painter = QtGui.QPainter()
         self.painter.begin(self)
@@ -31,12 +19,8 @@ class WFlicker(QtWidgets.QWidget):
         self.painter.setBrush(QtGui.QBrush(QtGui.QColor(0, 0, 0, self.opac), QtCore.Qt.SolidPattern))
  
         self.painter.drawRect(0, 0, self.flickerWidth, self.flickerHeight)
-        #self.painter.drawRect(-100, -100, 1000, 1000)
         self.painter.end()
     def refreshState(self, opac):
         print('refresh', opac)
         self.opac = opac
-
-    # def resizeEvent(self, e):
-    #     self.light.setGeometry(0, 0, self.width(), self.height())
 
--- Plugins/flicker.py
+++ Plugins/flicker.py
@@ -11,40 +11,40 @@ class Task(QtWidgets.QWidget):
         self.frameNumber = 0
         self.height = self.parent().height()
         self.width = self.parent().width()
-        #self.refresh_rate = self.parent().screen_refreshrate
-        # TRACK PARAMETERS ###
+        #self.refresh_rate = self.parent().screen_refreshrate #not needed at the moment
+
+
+        # FlickerPARAMETERS ###
         self.parameters = {
             'taskplacement': 'fullscreen',
-            'frequency': 10,
-            'taskupdatetime': 1,
+            'frequency': 100,        # flicker frequency to be devided by 10 to yield  Hz
+            'taskupdatetime': 1,   # should stay set to 1 to insure highest posible refresh rate
             'title': 'flicker',
             'amplitude': 100,
-            'amp_reduction': 10,
-            'task_duration_min':1
+            'amp_reduction': 10, # amplitude depth in percent
+            'task_duration_min':6 # needs to be initalized to calculate the sine curve in advance
         }
 
-        n_seconds = self.parameters['task_duration_min'] * 60 + 100 
-        self.opac_vals = (1 + np.sin(2 * np.pi * self.parameters['frequency']  * np.arange(0,n_seconds,1/1000))) * (self.parameters['amplitude'] / 2)  
-       # self.opac_vals = np.sin(2 * np.pi *  self.parameters['frequency'] * np.arange(0,n_seconds,1/1000)) * (self.parameters['amplitude'] / 2)  + self.parameters['amplitude'] / 2
-        self.opac_vals = self.opac_vals * (self.parameters['amp_reduction'] / 100)
-
     def onStart(self):
-        # Set a WTrack Qt object
-        self.widget = WFlicker.WFlicker(self, self.parameters['frequency'], self.parameters['amplitude'])
+        # Set a WFlicker Qt object
+        frequency = self.parameters['frequency'] / 10 #needs to happen due to how original script read parameters
+        n_seconds = self.parameters['task_duration_min'] * 60 + 100 # calculate the number of seconds the flicker should be presented for
+        self.opac_vals = (1 + np.sin(2 * np.pi * frequency  * np.arange(0,n_seconds,1/1000))) * (self.parameters['amplitude'] / 2)  # init sine wave
+        self.opac_vals = self.opac_vals * (self.parameters['amp_reduction'] / 100) # reduce amplitude of sine wave
+        self.widget = WFlicker.WFlicker(self) # init the widget
 
         # Create a layout for the widget
         layout = QtWidgets.QGridLayout()
 
-        # Add the WTrack object to the layout
+        # Add the WFlicker object to the layout
         layout.addWidget(self.widget)
         self.setLayout(layout)
 
 
     def onUpdate(self):
-        # Refresh the display
+        # Refresh the opacity of the flicker widget
         #print('refresh',self.parent().totalElapsedTime_ms, self.opac_vals[round(self.parent().totalElapsedTime_ms)])
-        self.frameNumber += 1
+        #self.frameNumber += 1
         self.widget.refreshState(self.opac_vals[round(self.parent().totalElapsedTime_ms )])
         self.widget.repaint()
-        #self.widget.refreshState(self.opac_vals[self.frameNumber])
 
